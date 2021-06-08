73a2bc67b07c2e0a7b103bed960e8df97e33586e
FinnFreiheit <finn.owe.freiheit@gmail.com>
2021-06-08 09:45:45 +0200

    StrategyFactory

--- src/main/java/Strategy/Context.java
+++ src/main/java/Strategy/Context.java
@@ -2,7 +2,14 @@ package Strategy;
 
 public class Context
 {
-    Strategy strategy = new DeutscheTVSender();
+    /**
+     * Instanz der Faktory. Faktory ließt Configfile und bestimmt die richtige Strategy durch
+     * getStrategy.
+     * Besser wenn Verwendung von Reflection. Dann muss name in der Config idetentisch mit Klassenname
+     * von Algorithmus. (name Config zB: DeutscheTVSender)
+     */
+    private final StrategyFactory strategyFactory = new StrategyFactory();
+    private Strategy strategy = strategyFactory.getStrategy();
 
     public void aggregatAnwenden(){
         strategy.executeAlgorithm();
